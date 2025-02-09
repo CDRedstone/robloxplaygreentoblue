5f8971c689246da62b89363e3460af30a287661b
CDRedstone <69321093+CDRedstone@users.noreply.github.com>
2025-02-09 16:10:03 -0600

    Add more selectors

--- main.js
+++ main.js
@@ -17,7 +17,7 @@
     var greencolor = "#56AC72";
     const style = document.createElement('style');
 style.innerHTML = `
-    .btn-common-play-game-lg, .btn-growth-sm, .btn-growth-md, .btn-toggle.on, .web-blox-css-tss-lo77dr-Button-contained, .web-blox-css-mui-1ujcvrb.Mui-checked + .MuiSwitch-track, .web-blox-css-tss-13ltd1g-Badge-colorPrimary, .web-blox-css-mui-23skhk, .web-blox-css-mui-1ghtbzf, .web-blox-css-tss-1ufr6a1-Tabs-indicator-tabsIndicator, .web-blox-css-tss-od6vvj-Alert-standardInfo {
+    .btn-common-play-game-lg, .btn-growth-sm, .btn-growth-md, .btn-toggle.on, .web-blox-css-tss-lo77dr-Button-contained, .web-blox-css-mui-1ujcvrb.Mui-checked + .MuiSwitch-track, .web-blox-css-tss-13ltd1g-Badge-colorPrimary, .web-blox-css-mui-23skhk, .web-blox-css-mui-1ghtbzf, .web-blox-css-tss-1ufr6a1-Tabs-indicator-tabsIndicator, .web-blox-css-tss-od6vvj-Alert-standardInfo, .cart-number-container {
         background-color: #01b270 !important;
     }
     .web-blox-css-mui-1ujcvrb.Mui-checked, .web-blox-css-tss-13r9j67-Link-colorPrimary-Link-root-preferenceLink, .web-blox-css-mui-k81yot, .web-blox-css-tss-q4r4zc-Link-colorPrimary-Link-root-experienceTitle, .web-blox-css-tss-hl1oq9-Link-colorPrimary-Link-root, .web-blox-css-tss-12qcr6e-Button-text, .web-blox-css-mui-4kistw, .web-blox-css-tss-d0yocm-Typography-overline-Typography-root-tableHeader, .web-blox-css-tss-1h34wph-Link-colorPrimary-Link-root-link {
@@ -28,6 +28,9 @@ style.innerHTML = `
     }
     #upgrade-now-button {
         background-color: #fff !important;
+    }
+    .icon-radio-check-circle-filled {
+        border: 6px solid #01b270 !important;
     }
 `;
 document.head.appendChild(style);
